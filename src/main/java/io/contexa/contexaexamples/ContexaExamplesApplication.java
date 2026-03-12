881face993ba4306d7e33b8a45a7bc448961380e
onjsdnjs <onjsdnjs@gmail.com>
2026-03-12 13:52:21 +0900

    Initial commit

---
+++ src/main/java/io/contexa/contexaexamples/ContexaExamplesApplication.java
@@ -0,0 +1,13 @@
+package io.contexa.contexaexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContexaExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContexaExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/contexa/contexaexamples/ContexaExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.contexa.contexaexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContexaExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
