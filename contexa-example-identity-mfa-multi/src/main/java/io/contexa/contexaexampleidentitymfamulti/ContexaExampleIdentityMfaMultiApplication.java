306627905656b1aa71888fadd0d97aa583b6f272
onjsdnjs <onjsdnjs@gmail.com>
2026-03-17 16:10:13 +0900

    contexa examples

---
+++ contexa-example-identity-mfa-multi/src/main/java/io/contexa/contexaexampleidentitymfamulti/ContexaExampleIdentityMfaMultiApplication.java
@@ -0,0 +1,13 @@
+package io.contexa.contexaexampleidentitymfamulti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContexaExampleIdentityMfaMultiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContexaExampleIdentityMfaMultiApplication.class, args);
+    }
+
+}
