3d5561610335499abeaa8c7673697c2f18563b80
onjsdnjs <onjsdnjs@gmail.com>
2026-03-19 18:11:34 +0900

    contexa examples

---
+++ contexa-examples-legacy/src/main/java/io/contexa/contexaexampleslegacy/ContexaExamplesLegacyApplication.java
@@ -0,0 +1,13 @@
+package io.contexa.contexaexampleslegacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContexaExamplesLegacyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContexaExamplesLegacyApplication.class, args);
+    }
+
+}
---
+++ contexa-examples-legacy/src/test/java/io/contexa/contexaexampleslegacy/ContexaExamplesLegacyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.contexa.contexaexampleslegacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContexaExamplesLegacyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
